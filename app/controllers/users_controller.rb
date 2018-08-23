911b95d773a1b8422401cde2b264bb908035ae25
Nate Miller <NKDMiller96@gmail.com>
2018-08-23 11:55:19 -0500

    sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+	set :views, Proc.new { File.join(root, "../views/") }
 	get '/users' do
-		"Display Users Here"
+		erb :signup
+	end
+	post "/signup" do
+	  user = User.new(:name => params[:name], :password => params[:password])
+	  if user.save
+	    redirect "/login"
+	  else
+	    redirect "/failure"
+	  end
+	end
+	get '/users/login' do
+		"Display Login here"
+	end
+	get '/users/:id' do
+		"Display User Here"
 	end
 end
