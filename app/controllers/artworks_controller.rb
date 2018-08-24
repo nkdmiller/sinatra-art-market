ffad8bcabe49421ced9030df431c2c823cf28a6c
Nate Miller <NKDMiller96@gmail.com>
2018-08-24 00:06:36 -0500

    add artwork controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,23 @@
 class ArtworksController < ApplicationController
 	get '/artworks' do
-		"Display Artworks Here"
+		@create = false
+		erb:'artworks/show'
 	end
+	get '/artworks/new' do
+		erb:'artworks/new', :layout => false
+	end
+	post "/artworks" do
+		@create = true
+		creator_id, photo, image_path, quantity, buyer_id, price, description
+	  @artwork = Artwork.create(:name => params[:artwork][:name], :photo => params[:artwork][:name], :quantity => params[:artwork][:quantity], :price => params[:artwork][:price], :description => params[:artwork][:description], :creator_id => params[:artwork][:creator_id])
+	  @artwork.save
+	  redirect to '/artworks'
+	end
+	get '/artworks/:id/buy' do
+		erb:'artworks/buy', :layout => false
+	end
+	get '/artworks/:id/edit' do
+		erb:'artworks/edit', :layout => false
+	end	
+
 end
